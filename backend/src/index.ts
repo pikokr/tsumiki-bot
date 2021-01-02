f29094348786118057b03e94babf2167257c65bc
pikokr <parangee9706@gmail.com>
2021-01-02 16:09:15 +0900

    feat: register login mutation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,19 @@ import { ApolloServer } from 'apollo-server'
 import 'reflect-metadata'
 import dotenv from 'dotenv'
 import { buildSchema } from 'type-graphql'
-import { QueryResolver } from './resolvers/QueryResolver'
+import { DefaultResolver } from './resolvers/DefaultResolver'
 import path from 'path'
 
 dotenv.config({ path: path.join(process.cwd(), '../.env') })
 ;(async () => {
   const schema = await buildSchema({
-    resolvers: [QueryResolver],
+    resolvers: [DefaultResolver],
   })
   const server = new ApolloServer({
     schema,
+    context: ({ req }) => {
+      return { test: 123 }
+    },
   })
   server.listen(process.env.BACKEND_PORT, () => console.log('Listening'))
 })()
