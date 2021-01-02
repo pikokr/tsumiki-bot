979b4ecdb78f66e1d9b9459c02c985dc08ee7759
pikokr <parangee9706@gmail.com>
2021-01-02 12:19:23 +0900

    feat: add command arg on help command

--- bot/src/commands/general/help.ts
+++ bot/src/commands/general/help.ts
@@ -13,7 +13,7 @@ const categories = [
 export default class Help extends Command {
   constructor() {
     super("general__help", {
-      aliases: ["도움말", 'help', '도움'],
+      aliases: ["도움말", "help", "도움"],
       category: "general",
       args: [
         {
@@ -21,13 +21,21 @@ export default class Help extends Command {
           type: "string",
           default: null,
         },
+        {
+          id: "command",
+          type: "string",
+          default: null,
+        },
       ],
     });
   }
 
-  async exec(msg: Message, { category }: { category: string }) {
+  async exec(
+    msg: Message,
+    { category, command }: { category: string; command: string }
+  ) {
     const embed = new MessageEmbed();
-    if (!category || !categories.find(it => it.name === category)) {
+    if (!category || !categories.find((it) => it.name === category)) {
       embed.setTitle("도움말");
       this.client.commandHandler.categories.keyArray().forEach((it) => {
         const c = categories.find((r) => r.id === it);
@@ -36,15 +44,26 @@ export default class Help extends Command {
         }
       });
     } else {
-        const c = categories.find(it => it.name === category)!
-        embed.setTitle(c.name)
-        embed.setDescription(c.description)
-        embed.addFields(this.handler.findCategory(c.id).map(it => {
+      const c = categories.find((it) => it.name === category)!;
+      if (!command || !this.handler.findCategory(c.id).find(r=>r.id===command || r.aliases.includes(command))) {
+        embed.setTitle(c.name);
+        embed.setDescription(c.description);
+        embed.addFields(
+          this.handler.findCategory(c.id).map((it) => {
             return {
-                name: it.aliases[0],
-                value: it.description || '설명 없음'
-            }
-        }))
+              name: it.aliases[0],
+              value: it.description || "설명 없음",
+              inline: true
+            };
+          })
+        );
+        embed.setFooter('더 자세히 알아보려면 ]도움말 <카테고리> <명령어>를 입력해주세요')
+      } else {
+          const cmd = this.handler.findCategory(c.id).find(r=>r.id===command || r.aliases.includes(command))!
+          embed.setTitle(`${c.name} - ${cmd.aliases[0]}`)
+          embed.setDescription(cmd.description || '설명 없음')
+          embed.addField('별칭', cmd.aliases.map(it => '`' + it + '`').join(', '), true)
+      }
     }
     await msg.reply(embed);
   }
