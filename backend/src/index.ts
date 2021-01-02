f2b7a19660dd65b190307330262114ed9aefc221
pikokr <parangee9706@gmail.com>
2021-01-02 13:51:32 +0900

    feat: apollo server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world!')
+import { ApolloServer } from 'apollo-server'
+import 'reflect-metadata'
+import dotenv from 'dotenv'
+import { buildSchema } from 'type-graphql'
+import { QueryResolver } from './resolvers/QueryResolver'
+import path from 'path'
+
+dotenv.config({ path: path.join(process.cwd(), '../.env') })
+;(async () => {
+  const schema = await buildSchema({
+    resolvers: [QueryResolver],
+  })
+  const server = new ApolloServer({
+    schema,
+  })
+  server.listen(process.env.BACKEND_PORT, () => console.log('Listening'))
+})()
