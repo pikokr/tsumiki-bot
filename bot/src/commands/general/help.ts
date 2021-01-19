258dbdc69f8daf1ca8ef592671a11d73b051074a
pikokr <parangee9706@gmail.com>
2021-01-19 14:15:54 +0900

    feat: i18n

--- bot/src/commands/general/help.ts
+++ bot/src/commands/general/help.ts
@@ -5,13 +5,13 @@ import i18next from 'i18next'
 export default class extends Command {
   constructor() {
     super('help', {
-      aliases: ['도움말', 'help']
+      aliases: ['도움말', 'help', '도움']
     })
   }
 
   async exec(msg: Message) {
-    const embed = new MessageEmbed()
-      .setDescription('도움말')
+    if (!msg.util) return
+    const embed = new MessageEmbed().setTitle(msg.util.t('common:help.title'))
     return msg.reply(embed)
   }
 }
